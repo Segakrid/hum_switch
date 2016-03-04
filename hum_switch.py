ce67fb7e6cd457f46b665ca2ca261eac5fef1991
Dirk Ages <Dirk@Dirk-Agess-MacBook-Pro.local>
2016-03-04 20:32:21 +0100

    Changed while true loop in for i in range loop

--- hum_switch.py
+++ hum_switch.py
@@ -43,7 +43,8 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).  
 # If this happens try again!
 
-while True:
+# while True: # uncomment when sleep is set to 60 sec
+for i in range(5):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
         print 'Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity)
